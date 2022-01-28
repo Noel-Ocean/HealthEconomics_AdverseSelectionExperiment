d9fbe1a8aceca1bd24f13d937d66ac5675225a64
Noel-Ocean <93688842+Noel-Ocean@users.noreply.github.com>
2022-01-28 10:37:16 +0800

    Update Adverse_Selection_Web.py

--- Adverse_Selection_Web.py
+++ Adverse_Selection_Web.py
@@ -19,8 +19,7 @@ page = st.selectbox("Please choose a page", ["About this Web App", "View/Downloa
 
 if page=='About this Web App':
     st.subheader("About this Web App")
-    st.write("This web app is run as a seminar activity for Health Economics.")
-    st.write("The activity mimics the consumer decision making, price rises and market collapse in heatlh insurance market, illustrating the concept of Adverse Selection.")
+    st.write("This web app is run as a seminar activity for Health Economics. The activity mimics the consumer decision making, price rises and market collapse in heatlh insurance market, illustrating the concept of Adverse Selection.")
     st.write("**Adverse Selection** occurs as a result of asymmetric information of on the characteristics of the insured between the insurer and insured in the health insurance market. Individuals may have better idea of their risk status than does the insurance company. Insurer set upprice premium based on the “average” probability of adverse event.")
     st.write("Low risk individuals may find it difficult to obtain a fair insurance premium since the average premium is higher than their expected cost of future health care as shown in the dataset, so they drop out of the insurance pool. Once a low risk individual drops out, insurer makes a loss at the original premium, so he responds by adjusting the premium upwards to the “new average premium”.") 
     st.write("The higher the new average premium, the more individuals drop out since their expected cost of future health care is lower than the new average premium, leaving only the high risk individuals in the insurance pool who find the premium becomes unaffordable until the market collapse – this is known as a death spiral. Just like what is shown in the dataset that the average premium of Gold package is getting higher when healthy insured starts to drop out. It is also noticed in the data that if the insurance package is separated to the young and the old group, the premium for the old will be very expensive.")
@@ -53,7 +52,7 @@ elif page=="Run Experiment":
     
     Gold=[]
     for i,j in zip(range(1,31), df["Gold($)"]):
-        Gold.append(f"Character{i}, ${j}")
+        Gold.append(f"C{i}, ${j}")
 
     Bronze_plan =  st.multiselect('Bronze', options=Bronze, default=Bronze, key=int)
     Silver_plan = st.multiselect('Silver', options=Silver, default=Silver, key=int) 
