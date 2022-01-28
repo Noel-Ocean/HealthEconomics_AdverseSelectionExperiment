9e16820be7aa6f2f6973156f00bfc52428790b51
Noel-Ocean <93688842+Noel-Ocean@users.noreply.github.com>
2022-01-28 10:42:11 +0800

    Update Adverse_Selection_Web.py

--- Adverse_Selection_Web.py
+++ Adverse_Selection_Web.py
@@ -14,7 +14,7 @@ st.set_page_config(layout="wide")
 st.sidebar.write("**CMED6902 Health Economics**")
 st.sidebar.write("Adverse Selection: A Classroom Experiment")
 st.sidebar.write("**Reference**: Hodgson, A. (2014). Adverse Selection in Health Insurance Markets: A Classroom Experiment. The Journal of Economic Education, 45(2), 90-100.")
-st.sidebar.writer("**Web app developer source code (Python) on GitHub**: https://raw.githubusercontent.com/Noel-Ocean/HealthEconomics_AdverseSelectionExperiment/main/Adverse_Selection_Web.py")
+st.sidebar.writer("**Web app developer source code (Python) on GitHub**:")
 
 page = st.selectbox("Please choose a page", ["About this Web App", "View/Download Dataset", "Run Experiment"])
 
