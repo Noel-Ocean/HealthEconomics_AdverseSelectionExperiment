efc273575169728483457e3a9321b6903a9c8666
Noel-Ocean <93688842+Noel-Ocean@users.noreply.github.com>
2022-01-28 12:12:12 +0800

    Update Adverse_Selection_Web.py

--- Adverse_Selection_Web.py
+++ Adverse_Selection_Web.py
@@ -61,9 +61,9 @@ elif page=="Run Experiment":
     for i,j in zip(range(1,31), df["Gold($)"]):
         Gold.append(f"C{i}, ${j}")
 
-    Bronze_plan =  st.multiselect('Bronze', options=Bronze, default=Bronze, key=int)
-    Silver_plan = st.multiselect('Silver', options=Silver, default=Silver, key=int) 
-    Gold_plan = st.multiselect('Gold', options=Gold, default=Gold, key=int) 
+    Bronze_plan =  st.multiselect('Bronze (C1="Character1")', options=Bronze, default=Bronze, key=int)
+    Silver_plan = st.multiselect('Silver (C1="Character1")', options=Silver, default=Silver, key=int) 
+    Gold_plan = st.multiselect('Gold (C1="Character1")', options=Gold, default=Gold, key=int) 
 
     Bronze_cal = []
     for i in Bronze_plan:
