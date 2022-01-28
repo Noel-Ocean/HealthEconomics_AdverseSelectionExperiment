7702eb26657056cbb7bee7347e6ec3f98bcf0561
Noel-Ocean <93688842+Noel-Ocean@users.noreply.github.com>
2022-01-28 23:07:17 +0800

    Update Adverse_Selection_Web.py

--- Adverse_Selection_Web.py
+++ Adverse_Selection_Web.py
@@ -81,7 +81,7 @@ elif page=="Run Experiment":
         Gold_cal.append(int(find[0]))
 
     Bronze_average, Silver_average, Gold_average = round(sum(Bronze_cal)/30, round(sum(Silver_cal)/30), round(sum(Gold_cal)/30)
-    Bronze_raw, Silver_raw, Gold_raw = sum(df["Bronze"])/30, sum(df["Silver"])/30, sum(df["Gold"])/30   
+    Bronze_raw, Silver_raw, Gold_raw = sum(df["Bronze($)"])/30, sum(df["Silver($)"])/30, sum(df["Gold($)"])/30   
     AVERAGE, RAW = [Bronze_average, Silver_average, Gold_average], Bronze_raw, Silver_raw, Gold_raw
 
     st.write("**Please observe the AVERAGE prices:** Bronze=${}, Silver=${}, Gold=${}".format(Bronze_average, Silver_average, Gold_average))
